1ae9f2b356d5825d86d636f45eeff368b96e0897
unknown <Daryl@D20tower.scratch.local>
2015-08-26 10:59:27 -0400

    Improved parallax function

--- js/main.js
+++ js/main.js
@@ -7,18 +7,20 @@ function moveElement(element, speed, range){
 	}
 }
 
-function parallax(){
-	var parallax_diminish = 3;
-	var element = '.parallax';
+function parallax() {
+	//Make sure that the background-image you use with this function has a 
+	//significantly greater height than its container, or is a repeating pattern.
 
-	var scroll_distance = $(document).scrollTop();
-	var distance_from_top = $(element).offset().top;
-	var parallax_offset = ( ( scroll_distance + distance_from_top ) / parallax_diminish).toString();
-	var parallax_offset = Math.round(parallax_offset);
+	var strength = 0.5; //Effect Strength. Range: 0 - 1
+	var element = '.parallax'; //Elements to target. Seperate with comma
 
-	$(element).css('background-size', '110%');
-	$(element).css('background-position-x', 'center');
-	$(element).css('background-position-y', parallax_offset + 'px' );
+	var scroll_dist = $(document).scrollTop();
+	var dist_from_top = $(element).offset().top;
+	var offset = ( ( scroll_dist + dist_from_top ) * strength).toString();
+	var offset = Math.round(offset);
+
+	//$(element).css('background-position-x', 'center');
+	$(element).css('background-position-y', offset + 'px' );
 }
 
 $(document).ready(function() {
